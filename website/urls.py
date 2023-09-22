378cb7b219550b5aa95fa8dfff8773af699d5789
demo-23home <zeyadslama23@gmail.com>
2023-09-23 01:04:51 +0300

    creating Book Model & User & AdminUser

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from .views import *
-
+# 
 
 app_name = 'website'
 
 
 urlpatterns = [
     path('',home,name='home'),
-    # path('',login,name='login'),
     path('logout/',user_logout,name='logout'),
     path('register/',register,name='register'),
     path('record/<int:pk>',book_detail,name='book_detail'),
